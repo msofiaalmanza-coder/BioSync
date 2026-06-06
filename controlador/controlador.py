205c5433339c8d42abef4377e82c76125a86b462
luzdoria-lab <luz.doria@udea.edu.co>
2026-06-06 17:01:14 -0500

    primera parte controlador

---
+++ controlador/controlador.py
@@ -0,0 +1,33 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMessageBox
+from modelo.modelo_db import ModeloDB
+from modelo.modelo_dicom import DicomModel
+from modelo.modelo_señales import SenalesModel
+from modelo.modelo_tabular import ModeloTabular
+
+
+class Controlador:
+
+    def _init_(self):
+
+        self.main = uic.loadUi(
+            "vista/main.ui"
+        )
+
+        self.login = uic.loadUi(
+            "vista/login.ui"
+        )
+
+        self.principal = uic.loadUi(
+            "vista/principal.ui"
+        )
+
+        self.modelo_db = ModeloDB()
+
+        self.modelo_dicom = DicomModel()
+
+        self.modelo_senales = SenalesModel()
+
+        self.modelo_tabular = ModeloTabular()
+
+        self.conectar_eventos()
