07e70aa122646c00e13f9cad5d51707d861bfcbd
nataliava-06 <nataliavergaraa@udea.edu.co>
2026-06-06 17:30:19 -0500

    cambios vista main

--- Vista/vista.py
+++ Vista/vista.py
@@ -7,6 +7,6 @@ class VistaBienvenida(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        uic.loadUi("Vista/bienvenida.ui", self)
+        uic.loadUi("main.ui", self)
 
-        self.setWindowTitle("BioMed Analyzer")
+        self.setWindowTitle("BioSync")
