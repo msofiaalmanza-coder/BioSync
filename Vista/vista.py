6333c681c387761e8aef25af56281f6c100256a5
nataliava-06 <nataliavergaraa@udea.edu.co>
2026-06-06 17:22:02 -0500

    vista de la pagina principal

--- Vista/vista.py
+++ Vista/vista.py
@@ -1,18 +1,12 @@
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
 
-class VistaLogin(QMainWindow):
+
+class VistaBienvenida(QMainWindow):
+
     def __init__(self):
         super().__init__()
-        uic.loadUi("vista/login.ui", self)
-        self.btn_ingresar.clicked.connect(self.ingresar)
-        self.callback = None
 
-    def ingresar(self):
-        usuario = self.input_usuario.text()
-        contrasena = self.input_contrasena.text()
-        if self.callback:
-            self.callback(usuario, contrasena)
+        uic.loadUi("Vista/bienvenida.ui", self)
 
-    def mostrar_error(self, msg):
-        self.lbl_error.setText(msg)
+        self.setWindowTitle("BioMed Analyzer")
