2fb8258d6d79f021f860192324c85c3d028be288
nataliava-06 <nataliavergaraa@udea.edu.co>
2026-06-07 22:46:05 -0500

    añadi un archivo dicom anonimo de prueba

--- controlador/controlador.py
+++ controlador/controlador.py
@@ -185,7 +185,7 @@ class Controlador:
             return
         corte = self.modelo_dicom.corte_coronal(indice)
         norm = self.modelo_dicom.normalizar(corte)
-        norm = np.flipud(norm)
+        norm = np.rot90(np.flipud(norm))
         self.mostrar_imagen(norm, self.principal.lblCoronal_2)
 
     def actualizar_sagital(self, indice):
@@ -193,7 +193,7 @@ class Controlador:
             return
         corte = self.modelo_dicom.corte_sagital(indice)
         norm = self.modelo_dicom.normalizar(corte)
-        norm = np.flipud(norm)
+        norm = np.rot90(np.flipud(norm))
         self.mostrar_imagen(norm, self.principal.lblSagital_2)
 
     def convertir_nifti(self):
